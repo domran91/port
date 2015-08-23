40632f182a94314ab78161a7bee55e435fb74baa
domran91 <dom.randolph@gmail.com>
2015-08-23 16:06:10 -0500

    Updated
    Updated Welcome Page

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,54 @@
+@extends('master')
+
+@section('title', 'Dominique Randolph - Portfolio')
+
+
+
+@section('content')
+
+    <img src='images/port.jpg' class="img-responsive" style="min-width: 100%;"></img>
+
+
+<div class="container">
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+        <div class="col-md-7">
+            <h2 class="featurette-heading">Resume Creation. <span class="text-muted">Easy as answering a few questions.</span></h2>
+            <p class="lead">
+                This website is created by Dominique Randolph for practice using Laravel + Bootstrap. So take a moment to create an account. Then take all your life experiences and achievements and add them to your profile. Then your resume is automaticly generated.
+            </p>
+        </div>
+        <div class="col-md-5">
+            <img class="featurette-image img-responsive center-block" src="/images/step_one.jpg" alt="Generic placeholder image">
+        </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+            <h2 class="featurette-heading">Scope + MVC. <span class="text-muted">See for yourself.</span></h2>
+            <p class="lead">You can visit the <b>Github</b> <a href="https://github.com/domran91/port">repository</a> to see all the code that was created. There are classes to handel each form of information and views to each page view.</p>
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+            <img class="featurette-image img-responsive center-block" src="/images/step_two.jpg" alt="Generic placeholder image">
+        </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+        <div class="col-md-7">
+            <h2 class="featurette-heading">Why I created this site? <span class="text-muted">Simple Presentation.</span></h2>
+            <p class="lead">While I have worked on many projects not many are live right now. So I this site to demonstrate my programming skills and at the same time present to you my resume. Thank you for considering me for you job position. Now lets take the next step and start planning and creation the next big thing together.</p>
+        </div>
+        <div class="col-md-5">
+            <img class="featurette-image img-responsive center-block" src="/images/step_three.jpg" alt="Generic placeholder image">
+        </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+</div>
+@stop
